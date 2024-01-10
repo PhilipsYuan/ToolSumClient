4220b6cd9f8def139a4bd29c0581851b633630f7
yuan.fei <yuan.fei@smartcompany.jp>
2024-01-10 11:49:02 +0800

    优化tencentTV

--- src/main/videoDownload/analysis/analysisLink/analysisByPlatform/tencentTvBack/tencentTVBack.js
+++ src/main/videoDownload/analysis/analysisLink/analysisByPlatform/tencentTvBack/tencentTVBack.js
@@ -1,9 +1,15 @@
 import {app, BrowserWindow} from "electron";
 import puppeteer from "../../../../../util/source/puppeteer-core";
-import axios from "../../../../../util/source/axios";
 import path from "path";
 import {makeDir} from "../../../../../util/fs";
 import fs from "fs";
+import {getPlayList} from "../../../../../util/m3u8Parse";
+
+/**
+ * 如果是免费的视频，m3u8文件在proxyhttp直接获取。
+ * 如果是付费的视频，m3u8文件在ul下面获取接口，重新获取。独立的http请求
+ * @type {string}
+ */
 
 const basePath = app.getPath('userData')
 const tempM3u8UrlPath = path.resolve(basePath, 'm3u8Video', 'tempM3u8Url');
@@ -12,34 +18,19 @@ const m3u8UrlMgPath = path.resolve(tempM3u8UrlPath, 'tencent')
 makeDir(m3u8UrlMgPath)
 
 export function getTencentTVDownloadLink(htmlUrl) {
-    const promise1 = getM3u8Link(htmlUrl)
-    const promise2 = getTitle(htmlUrl)
-    return Promise.all([promise1, promise2])
-        .then((results) => {
-            if(results[0] === 'error' || results[1] === 'error') {
+    return getM3u8Link(htmlUrl)
+        .then((result) => {
+            if(result === 'error') {
                 return 'error'
             } else {
-                return {title: results[1], videoUrl: results[0]}
+                return {title: result.title, videoUrl: result.m3u8Url}
             }
         })
 }
 
-export function getTitle(htmlUrl) {
-    return axios
-        .get(htmlUrl)
-        .then((res) => {
-            const data = res.data
-            const title = data.match(/<title>(.*)<\/title>/)?.[1].split('-')[0].trim();
-            return title
-        })
-        .catch(() => {
-            return 'error'
-        })
-}
-
-
 async function getM3u8Link(htmlUrl) {
     let m3u8Url = null
+    let title = null
     const browser = await pie.connect(app, puppeteer);
     const window = new BrowserWindow({
         show: false, width: 900, height: 600, webPreferences: {
@@ -61,7 +52,8 @@ async function getM3u8Link(htmlUrl) {
             const json2 = JSON.parse(json.vinfo)
             if(json2?.vl?.vi[0].ul?.m3u8) {
                 const vid = getVid(htmlUrl)
-                m3u8Url = await createM3u8Url(json2?.vl?.vi[0].ul?.m3u8, vid)
+                m3u8Url = await createM3u8Url(json2?.vl?.vi[0].ul?.m3u8, vid, json2?.vl?.vi[0].ul.ui[0].url)
+                title = json2?.vl?.vi[0].ti
             }
         }
     }
@@ -80,7 +72,7 @@ async function getM3u8Link(htmlUrl) {
                             page.removeListener('response', responseFun);
                             clearInterval(interval);
                             window && window.destroy();
-                            resolve(m3u8Url)
+                            resolve({m3u8Url, title})
                         } else {
                             index++
                         }
@@ -101,7 +93,12 @@ async function getM3u8Link(htmlUrl) {
 /**
  * 创建本地m3u8url
  */
-async function createM3u8Url(m3u8Text, id) {
+async function createM3u8Url(m3u8Text, id, hostPath) {
+    const allUrls = getPlayList(m3u8Text)
+    allUrls.forEach((item) => {
+        const url = buildAbsoluteURL(hostPath, item)
+        m3u8Text = m3u8Text.replace(item, url)
+    })
     const json = {
         text: m3u8Text
     }
@@ -118,4 +115,82 @@ async function createM3u8Url(m3u8Text, id) {
 function getVid (htmlUrl) {
     const vid = htmlUrl.match(/cover\/[^\/]*\/([^.]*).html/)[1]
     return vid
+}
+
+/**
+ * 创建绝对路径
+ */
+function buildAbsoluteURL(e, t, r= {alwaysNormalize: true}) {
+    const n = /^([^\/?#]*)([^]*)$/;
+    if (r = r || {},
+        e = e.trim(),
+        !(t = t.trim())) {
+        if (!r.alwaysNormalize)
+            return e;
+        const i = parseURL(e);
+        if (!i)
+            throw new Error("Error trying to parse base URL.");
+        return i.path = normalizePath(i.path),
+            buildURLFromParts(i)
+    }
+    var a = parseURL(t);
+    if (!a)
+        throw new Error("Error trying to parse relative URL.");
+    if (a.scheme)
+        return r.alwaysNormalize ? (a.path = normalizePath(a.path),
+            buildURLFromParts(a)) : t;
+    var o = parseURL(e);
+    if (!o)
+        throw new Error("Error trying to parse base URL.");
+    if (!o.netLoc && o.path && "/" !== o.path[0]) {
+        var l = n.exec(o.path);
+        o.netLoc = l[1],
+            o.path = l[2]
+    }
+    o.netLoc && !o.path && (o.path = "/");
+    var c = {
+        scheme: o.scheme,
+        netLoc: a.netLoc,
+        path: null,
+        params: a.params,
+        query: a.query,
+        fragment: a.fragment
+    };
+    if (!a.netLoc && (c.netLoc = o.netLoc,
+    "/" !== a.path[0]))
+        if (a.path) {
+            var h = o.path
+                , u = h.substring(0, h.lastIndexOf("/") + 1) + a.path;
+            c.path = normalizePath(u)
+        } else
+            c.path = o.path,
+            a.params || (c.params = o.params,
+            a.query || (c.query = o.query));
+    return null === c.path && (c.path = r.alwaysNormalize ? normalizePath(a.path) : a.path),
+        buildURLFromParts(c)
+}
+
+function parseURL (e) {
+    const i = /^((?:[a-zA-Z0-9+\-.]+:)?)(\/\/[^\/?#]*)?((?:[^\/?#]*\/)*[^;?#]*)?(;[^?#]*)?(\?[^#]*)?(#[^]*)?$/
+    let t = i.exec(e);
+    return t ? {
+        scheme: t[1] || "",
+        netLoc: t[2] || "",
+        path: t[3] || "",
+        params: t[4] || "",
+        query: t[5] || "",
+        fragment: t[6] || ""
+    } : null
+}
+
+function normalizePath(e) {
+    const a = /(?:\/|^)\.(?=\/)/g;
+    const o = /(?:\/|^)\.\.\/(?!\.\.\/)[^\/]*(?=\/)/g;
+    for (e = e.split("").reverse().join("").replace(a, ""); e.length !== (e = e.replace(o, "")).length; )
+        ;
+    return e.split("").reverse().join("")
+}
+
+function buildURLFromParts(e) {
+    return e.scheme + e.netLoc + e.path + e.params + e.query + e.fragment
 }
