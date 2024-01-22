e298c98e960223819621d92a38b25a0c611dd9f2
cindy <289325521@qq.com>
2024-01-22 22:08:49 +0800

    补充解析vip视频

---
+++ src/main/videoDownload/analysis/analysisLink/analysisByPlatform/iqiyiTV/iqiyi.md
@@ -0,0 +1,15 @@
+### iqiyi的请求数据，靠接口： https://cache.video.iqiyi.com/dash？
+
+### vip会员的破解方式：
+1. 在数据库里存储 cookie，因为爱奇艺的cookie可以保存3个月。所以只需要每三个月更新下cookie就可以了。
+2. 爱奇艺里的cookie值，只有P00001 和 IMS 参数有用。（）
+
+
+### cookie的例子
+1. P00001=0a0m1pRim2YElm1wCr3JJr727JwZ4lKTkt1rAIVe2m2fF3sJKFyDA5zI5Xaqc5jF95Hm13W12; IMS=IggQABj_obqtBioyCiAzNzBhNTY5YTUxZTUxMGY5MGYzYjU1ZmMwNDg5ODNkYRAAIggI0AUQAhiwCShOMAUwADAAciQKIDM3MGE1NjlhNTFlNTEwZjkwZjNiNTVmYzA0ODk4M2RhEAA;
+
+
+### iqiyi待解决的问题
+1. 验证下，只用cookie可否在多台设备上下载
+2. cookie的时间(3个月)是否真的好用
+3. 需要将cookie的接口，进行加密和解密
--- src/main/videoDownload/analysis/analysisLink/analysisByPlatform/iqiyiTV/iqiyiTV.js
+++ src/main/videoDownload/analysis/analysisLink/analysisByPlatform/iqiyiTV/iqiyiTV.js
@@ -13,13 +13,15 @@ makeDir(m3u8UrlMgPath)
 
 export async function getIQiYiTVDownloadLink (htmlUrl) {
     const { tvId, vid, title, payMark } = await getVid(htmlUrl)
-    // if(payMark == 1) {
-    //
-    // } else  if(payMark == 0) {
-    //     return getFreeVideo(tvId, title)
-    // } else {
-    //     return 'error'
-    // }
+    if(payMark == 1) {
+        const result  = await axios.get('http://localhost:8083/mini/systemConfig/ic')
+        console.log(result.data.result.cookie)
+        return getFreeVideo(tvId, title, result.data.result.cookie)
+    } else  if(payMark == 0) {
+        return getFreeVideo(tvId, title)
+    } else {
+        return 'error'
+    }
     return getFreeVideo(tvId, title)
 }
 
@@ -36,7 +38,7 @@ function getVipVideo() {
  * @param title
  * @returns {*}
  */
-function getFreeVideo(tvId, title) {
+function getFreeVideo(tvId, title, cookie) {
     const authKey =  auth('')
     const bob = '{"version":"10.0","dfp":"a0ecf8e4c6312b4cb6b07659066e7caca9279d3d666b49de92ddf17cab892223b9","b_ft1":8}'
     const bobCode = bob.replace(/:/g, '%3A').replace(/,/g, '%2C').replace(/{/g, '%7B').replace(/"/g, '%22').replace(/}/g, '%7D')
@@ -92,9 +94,12 @@ function getFreeVideo(tvId, title) {
     temp = temp.substring(0, temp.length-1)
     const vfString = getVf(temp)
     const search = temp + '&vf=' + vfString
-    const apiURL = 'https://cache.video.iqiyi.com/dash?tvid=7147799058657300&bid=300&vid=cf161d7cee51d976a5694cb2e1ca3458&src=01080031010000000000&vt=0&rs=1&uid=1178183755904228&ori=pcw&ps=1&k_uid=80f0bd95358b2c98c3107bd7091ebd5b&pt=0&d=0&s=&lid=0&cf=0&ct=0&authKey=f80ef69dce63324be1f3d7c06a011691&k_tag=1&dfp=a0ecf8e4c6312b4cb6b07659066e7caca9279d3d666b49de92ddf17cab892223b9&locale=zh_cn&pck=74tVlx8g7TMZQ8CI64O6R6zyUnch7W6BQWqEWdBxnhJ8m28ay3fjsZujuBE31Ausj2w61&k_err_retries=0&up=&sr=1&qd_v=5&tm=1704874552266&qdy=u&qds=0&k_ft1=706436220846852&k_ft4=1162321298202628&k_ft2=262335&k_ft5=134217729&k_ft6=128&k_ft7=755499012&fr_300=120_120_120_120_120_120&fr_500=120_120_120_120_120_120&fr_600=120_120_120_120_120_120&fr_800=120_120_120_120_120_120&fr_1020=120_120_120_120_120_120&bop=%7B%22version%22%3A%2210.0%22%2C%22dfp%22%3A%22a0ecf8e4c6312b4cb6b07659066e7caca9279d3d666b49de92ddf17cab892223b9%22%2C%22b_ft1%22%3A8%7D&ut=1&vf=d209d6ce8d187e57a06ac5e04c735e0c'
-    // const apiURL = `https://cache.video.iqiyi.com${search}`
-    return axios.get(apiURL)
+    const apiURL = `https://cache.video.iqiyi.com${search}`
+    const headers = {}
+    if(cookie) {
+        headers.Cookie = cookie
+    }
+    return axios.get(apiURL, { headers })
         .then(async (res) => {
             const videos = res.data.data?.program?.video
             if(videos) {
@@ -118,7 +123,6 @@ function getVid(htmlUrl) {
                 'User-Agent':
                     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
                 referer: 'https://www.iqiyi.com',
-                Cookie: '__uuid=17895c5d-3ca8-bd54-be4b-02f217b41b73; P00001=74tVlx8g7TMZQ8CI64O6R6zyUnch7W6BQWqEWdBxnhJ8m28ay3fjsZujuBE31Ausj2w61; IMS=IggQABj_oICtBioyCiBjMmU2MzhkNWYyZmZlODQ2MjRmNjkxYjg1YWVlNzhmYRAAIggI0AUQAhiwCShFMAUwADAAMAByKAogYzJlNjM4ZDVmMmZmZTg0NjI0ZjY5MWI4NWFlZTc4ZmEQn5P-rAaCAQCKASQKIgogYzJlNjM4ZDVmMmZmZTg0NjI0ZjY5MWI4NWFlZTc4ZmE;'
             },
         })
         .then((res) => {
@@ -127,8 +131,6 @@ function getVid(htmlUrl) {
             const vid = data.match(/"vid":"(.*?)",/)[1]
             const title = data.match(/\.name="([^"]*)"/)[1];
             const payMark = data.match(/"payMark":(\d+),/)[1];
-            const urls = data.match(/"urls":\[([^\]]*)\]/)[1];
-            console.log(urls.split(',').length)
             return {tvId, vid, title, payMark}
         })
         .catch((e) => {
