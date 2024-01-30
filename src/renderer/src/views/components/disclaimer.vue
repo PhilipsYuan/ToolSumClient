35e6a85040e1579626fc1e5ebb3c58c3005a6f4e
yuan.fei <yuan.fei@smartcompany.jp>
2024-01-30 17:21:50 +0800

    免责申明

---
+++ src/renderer/src/views/components/disclaimer.vue
@@ -0,0 +1,31 @@
+<template>
+  <p-dialog v-model="showModal" title="免责申明" :show-footer="false" :show-close="true"
+            :destroy-on-close="true" :close-on-click-modal="false"
+            :close-on-press-escape="false" width="500px">
+  </p-dialog>
+</template>
+
+<script>
+import PDialog from "../UIComponents/PDialog.vue";
+export default {
+  name: "disclaimer",
+  components: { PDialog },
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    open () {
+      this.showModal = true
+    },
+    close () {
+      this.showModal = false
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
